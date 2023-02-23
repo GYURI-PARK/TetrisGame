1b88e8023430485334f0a9737b1a924e9958ceb1
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2023-02-24 02:28:33 +0900

    블럭1개 정의 완료 + 7개 만들기

--- TetrisGame/Background/Brick.swift
+++ TetrisGame/Background/Brick.swift
@@ -20,16 +20,61 @@ class Brick{
     }
     
     // 새로운 블럭을 가져오는 함수 정의
-    func bricks() -> Brick {
-        var bricks = Bricks()
+    func bricks() -> Brick{
+        var bricks = [Bricks]()
 
-        var brick1 = [CGPoint]()
-        
         // 하나의 테트리스 블럭 모양 정의
+        var brick1 = [CGPoint]()
         brick1.append(CGPoint(x: 0, y: 0)) // 각각의 블럭이 처음에 가지고 있는 위치값 집어넣기
         brick1.append(CGPoint(x: 1, y: 0))
         brick1.append(CGPoint(x: -1, y: 0))
         brick1.append(CGPoint(x: 0, y: 1))
-
+        bricks.append(Bricks(color: .red, points: brick1, brickName: "brick1"))
+        
+        var brick2 = [CGPoint]()
+        brick2.append(CGPoint(x: -1, y: 0))
+        brick2.append(CGPoint(x: 0, y: 0))
+        brick2.append(CGPoint(x: 1, y: 0))
+        brick2.append(CGPoint(x: 2, y: 0))
+        bricks.append(Bricks(color: .cyan, points: brick2, brickName: "brick2"))
+        
+        var brick3 = [CGPoint]()
+        brick3.append(CGPoint(x: 0, y: 0))
+        brick3.append(CGPoint(x: 1, y: 0))
+        brick3.append(CGPoint(x: 0, y: 1))
+        brick3.append(CGPoint(x: -1, y: 1))
+        bricks.append(Bricks(color: .green , points: brick3, brickName: "brick3"))
+        
+        var brick4 = [CGPoint]()
+        brick4.append(CGPoint(x: -1, y: 1))
+        brick4.append(CGPoint(x: -1, y: 0))
+        brick4.append(CGPoint(x: 0, y: 0))
+        brick4.append(CGPoint(x: 1, y: 0))
+        bricks.append(Bricks(color: .purple, points: brick4, brickName: "brick4"))
+        
+        var brick5 = [CGPoint]()
+        brick5.append(CGPoint(x: 1, y: 0))
+        brick5.append(CGPoint(x: 0, y: 0))
+        brick5.append(CGPoint(x: 1, y: -1))
+        brick5.append(CGPoint(x: 0, y: -1))
+        bricks.append(Bricks(color: .orange, points: brick5, brickName: "brick5"))
+        
+        var brick6 = [CGPoint]()
+        brick6.append(CGPoint(x: 1, y: 1))
+        brick6.append(CGPoint(x: -1, y: 0))
+        brick6.append(CGPoint(x: 0, y: 0))
+        brick6.append(CGPoint(x: 1, y: 0))
+        bricks.append(Bricks(color: .yellow, points: brick6, brickName: "brick6"))
+        
+        var brick7 = [CGPoint]()
+        brick7.append(CGPoint(x: 0, y: 0))
+        brick7.append(CGPoint(x: -1, y: 0))
+        brick7.append(CGPoint(x: 0, y: 1))
+        brick7.append(CGPoint(x: 1, y: 1))
+        bricks.append(Bricks(color: .systemPink, points: brick7, brickName: "brick7"))
+        
+        // 7개의 블럭들 중 랜덤으로 bricks에 넣어주기
+        let random = Int.random(in: 0..<7)
+        return bricks[random]
     }
 }
