f6b45dfa8af75b555823d09717e06fe7b2b2bf75
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-13 00:04:33 +0900

    left button 동작 확인 (5:46)

--- TetrisGame/Functions/LeftButton.swift
+++ TetrisGame/Functions/LeftButton.swift
@@ -14,6 +14,7 @@ class LeftButton {
         let btn = SKSpriteNode()
         btn.texture = SKTexture(imageNamed: "left_btn1")
         btn.size = CGSize(width: 50, height: 50)
+        btn.name = "left"
         btn.position = CGPoint(x: 50, y: -Int(Variables.scene.frame.height)+50)
         Variables.scene.addChild(btn)
     } // LeftButton정의 후 초기화 -> 메인 클래스에 LeftButton 인스턴스 생성
