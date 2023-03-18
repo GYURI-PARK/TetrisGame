b6b28db132b3d6c8adc2fbf351401e3cbc4ec512
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2023-03-18 13:46:47 +0900

    restart button -> main scene
    10:29

--- TetrisGame/Scene/GameOver.swift
+++ TetrisGame/Scene/GameOver.swift
@@ -11,4 +11,18 @@ import SpriteKit
 class GameOver: SKScene{
     
     // restart button 클릭 시 -> main scene으로 보내기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let location = touch?.location(in: self)
+        let node = nodes(at: location!)
+        for item in node{
+            if item.name == "restart"{
+                let scene = GameScene(size: view!.frame.size)
+                scene.scaleMode = .aspectFill
+                scene.anchorPoint = CGPoint(x: 0, y: 1)
+                let transition = SKTransition.moveIn(with: .left, duration: 1)
+                view!.presentScene(scene , transition: transition)
+            }
+        }
+    }
 }
